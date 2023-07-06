7fe8ffccab399d18cb816c8ad068386db5f9ca31
Randall Granier <programmingkitchen@gmail.com>
2023-07-06 08:29:54 -0400

    Removed comments from wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#from voteapp import app
 from voteapp import application
 
 if __name__ == '__main__':
-    #app.run(debug=True, host="0.0.0.0", port=5000)
     application.run()
